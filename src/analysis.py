b1df66bb7d27ae94d6d55c27717ce38a1c62e5de
adeli1234 <humanocreciendo@gmail.com>
2026-09-20 18:38:48 -0600

    Agrega visualizaciones, conclusiones y documentación
    Se agregan gráficas y conclusiones

--- src/analysis.py
+++ src/analysis.py
@@ -66,3 +66,54 @@ df['rendimiento'] = pd.cut(df['average_score'], bins=[0, 60, 80, 100], labels=la
 print("Clasificación de rendimiento:")
 print(df[['math_score', 'reading_score', 'writing_score', 'average_score', 'rendimiento']].head())
 print("-"*20)
+
+# ¿Cuál de las tres areas tiene el promedio más alto?
+average_math = df['math_score'].mean()
+average_reading = df['reading_score'].mean()
+average_writing = df['writing_score'].mean()
+
+plt.figure(figsize=(8, 6))
+plt.bar(['MATEMÁTICAS', 'LECTURA', 'ESCRITURA'], [average_math, average_reading, average_writing])
+plt.xlabel('Área')
+plt.ylabel('Promedio')
+plt.title('Promedio de Puntajes por Área')
+plt.savefig('outputs/resultados/promedio_areas.png')
+plt.close()
+plt.show()
+
+print("El area con el promedio más alto es:", end=" ")
+if average_math >= average_reading and average_math >= average_writing:
+    print("Matemáticas")
+elif average_reading >= average_writing:
+    print("LECTURA")
+else:
+    print("ESCRITURA")
+
+# ¿Qué porcentaje de estudiantes alcanzaron un rendimiento alto?
+high_perform_students = df[df['rendimiento'] == 'Alto']
+high_perform_stu_percentage = (len(high_perform_students) / len(df)) * 100
+print(f"El porcentaje de estudiantes con rendimiento alto es: {high_perform_stu_percentage:.2f}%")
+
+plt.figure(figsize=(8, 6))
+plt.pie(df['rendimiento'].value_counts(), labels=df['rendimiento'].value_counts().index, autopct='%1.1f%%', startangle=90)
+plt.title('Distribución de Rendimiento de Estudiantes')
+plt.savefig('outputs/resultados/distribucion_rendimiento.png')
+plt.close()
+plt.show()
+
+# ¿Los estudiantes que realizaron el curso de preparación presentan mejores resultados?
+prep_stu = df[df['test_preparation_course'] == 'completed']
+print(f"El promedio de puntajes para estudiantes que realizaron el curso de preparación fue de: {prep_stu['average_score'].mean():.2f}")
+
+# ¿Cuál es el promedio de puntajes para estudiantes que no realizaron el curso de preparación?
+no_prep_stu = df[df['test_preparation_course'] == 'none']
+print(f"El promedio de puntajes para estudiantes que no realizaron el curso de preparación fue de: {no_prep_stu['average_score'].mean():.2f}")
+
+plt.figure(figsize=(8, 6))
+plt.bar(['Curso de preparación completado', 'Curso de preparación no realizado'], [prep_stu['average_score'].mean(), no_prep_stu['average_score'].mean()])
+plt.xlabel('Curso de preparación')
+plt.ylabel('Promedio de puntajes')
+plt.title('Comparación de Promedio de Puntajes según Curso de Preparación')
+plt.savefig('outputs/resultados/comparacion_preparacion.png')
+plt.close()
+plt.show()
